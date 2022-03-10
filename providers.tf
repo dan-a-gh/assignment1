8bd6de0577518bedf3f20ef35a233d2d1fc4d7a0
Daniel Allison <contact@danielallison.dev>
2022-03-10 21:57:18 +0000

    Add tags in root file, formatting

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "2.98.0"
     }
   }
@@ -11,4 +11,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
